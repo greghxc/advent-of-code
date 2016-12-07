c0045200a8f53fed58797e64031cd4cf967af89c
Greg Mertzlufft <greg@bikebloc.com>
2016-12-07 13:10:45 -0800

    linting

--- main/day_07/lib.rb
+++ main/day_07/lib.rb
@@ -7,19 +7,20 @@ class IpAddress
 
   def tls?
     abba = /(.)(?!\1)(.)\2\1/
-    @p_vals.map { |v| v =~ abba }.compact.map{ |v| 
-      @n_vals.map { |n| !(n =~ abba) }.all? 
-    }.any?
+    @p_vals.map { |v| v =~ abba }.compact.map do
+      @n_vals.map { |n| !(n =~ abba) }.all?
+    end.any?
   end
 
   def ssl?
     aba = /(.)(?!\1)(.)\1/
-    @p_vals.flat_map do |v|
+    babs = @p_vals.flat_map do |v|
       (0..v.length).flat_map do |i|
         m = aba.match(v[i..-1])
         m ? m[2] + m[1] + m[2] : nil
       end
-    end.compact.uniq.flat_map do |p|
+    end.compact.uniq
+    babs.flat_map do |p|
       @n_vals.map { |n| n =~ /#{p}/ }.any?
     end.any?
   end
