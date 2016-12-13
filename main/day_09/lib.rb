9eabaaa5108cd045f3c0bf06853807f52873a535
Greg Mertzlufft <greg@bikebloc.com>
2016-12-12 20:18:46 -0800

    recursive

--- main/day_09/lib.rb
+++ main/day_09/lib.rb
@@ -18,27 +18,34 @@ end
 
 class DecompressorV2
   def self.length(input)
-    nr_len(input)
+    r_len(input)
   end
 
   private
 
   def self.r_len(input)
+    input = input.strip
     output_length = 0
     regex = /^\(([0-9]+)x([0-9]+)\)/
-
     i = 0
     until i >= input.length
-      regex.match(input[i..-1]) do |m|
+      m = regex.match(input[i..-1])
+      if m
+        new_input = input[m.offset(0)[1] + i..m.offset(0)[1].to_i + i - 1 + m[1].to_i]
+        output_length += r_len(new_input) * m[2].to_i
+        i += m.offset(0)[1] + m[1].to_i
+      else
+        i += 1
+        output_length += 1
       end
     end
+    output_length
   end
 
   def self.nr_len(input)
     mult = input.chars.map do |c|
       c =~ /[A-Z]/ ? 1 : 0
     end
-
     regex = /\(([0-9]+)x([0-9]+)\)/
     input.scan(regex) do |c|
       m = [c, Regexp.last_match.offset(0)[1]]
@@ -46,7 +53,6 @@ class DecompressorV2
         mult[i - 1] = mult[i - 1] * m[0][1].to_i
       end
     end
-
     mult.reduce(0, :+)
   end
 end
