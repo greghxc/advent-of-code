59278fc573fae035fd738c29f05a898b9cf5422c
Greg Mertzlufft <greghxc@dev.gregmertzlufft.com>
2016-12-21 23:04:07 -0500

    day 15

--- main/day_15/lib.rb
+++ main/day_15/lib.rb
@@ -6,11 +6,9 @@ class DiscStack
   def first_pass
     i = 0
     loop do
-      #puts "start time: #{i}"
       r = @discs.map.with_index do |d, s|
         d.clear?(i + s + 1)
       end
-      #puts r.inspect
       break if r.all?
       i += 1
     end
@@ -25,9 +23,6 @@ class Disc
   end
 
   def clear?(time)
-    #puts "#{@start} + #{time} % #{@positions} == 0"
-    #puts (@start + time) % @positions == 0
-    #sleep(0.5)
     (@start + time) % @positions == 0
   end
 end
